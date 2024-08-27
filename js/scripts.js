305eaf001cfd570cf6aecee5bfb2044df1d3929b
Alessio Vietri <info@alessiovietri.it>
2024-08-27 11:41:35 +0200

    Completato snack 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-console.log('JS collegato');
+/* 
+    Snack 1
+
+    Crea un array vuoto e chiedi all’utente un numero da inserire nell’array.
+    Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma non supera 50.
+    Alla fine, stampa l'array in console
+*/
+
+const myArr = [];
+let sum = 0;
+
+do {
+
+    const nextNumber = parseInt(prompt('Inserisci un numero:'));
+    console.log('Inserito:', nextNumber);
+
+    myArr.push(nextNumber);
+
+    sum += nextNumber;
+
+} while (sum <= 50);
+
+console.log(myArr);
