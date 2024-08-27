82d9844e12b91cf3790d9d329b96dc797d9ce89c
Alessio Vietri <info@alessiovietri.it>
2024-08-27 11:52:59 +0200

    Completato snack 2

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,55 @@
     Alla fine, stampa l'array in console
 */
 
-const myArr = [];
-let sum = 0;
+// const myArr = [];
+// let sum = 0;
 
-do {
+// do {
 
-    const nextNumber = parseInt(prompt('Inserisci un numero:'));
-    console.log('Inserito:', nextNumber);
+//     const nextNumber = parseInt(prompt('Inserisci un numero:'));
+//     console.log('Inserito:', nextNumber);
 
-    myArr.push(nextNumber);
+//     myArr.push(nextNumber);
 
-    sum += nextNumber;
+//     sum += nextNumber;
 
-} while (sum <= 50);
+// } while (sum <= 50);
 
-console.log(myArr);
+// console.log(myArr);
+
+/* 
+
+----------------------------------------------------------------------------------------------------
+
+*/
+
+/* 
+    Snack 2
+
+    Fai inserire un numero, che chiameremo N, all’utente.
+    Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.
+*/
+
+const arraysNumber = parseInt(prompt('Quanti array vuoi creare?'));
+console.log('Verranno creati', arraysNumber, 'array');
+
+const myArrays = [];
+
+for (let i = 0; i < arraysNumber; i++) {
+    console.log('Genero array', (i + 1));
+
+    const newArr = [];
+    for (let j = 0; j < 10; j++) {
+        const newNum = randomNum(1, 100);
+        newArr.push(newNum);
+    }
+    console.log('Array ' + (i + 1) + ':', newArr);
+
+    myArrays.push(newArr);
+}
+
+console.log('TUTTI i miei array:', myArrays);
+
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
